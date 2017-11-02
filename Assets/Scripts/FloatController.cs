af5cee1efcfd54246fd6929acacb85451653972a
= <a.widzinski@gmail.com>
2017-11-02 16:03:54 +0100

    Refactor kodu do mechanizmu walki z rybą

--- Assets/Scripts/FloatController.cs
+++ Assets/Scripts/FloatController.cs
@@ -5,86 +5,100 @@ using UnityEngine.UI;
 
 public class FloatController : MonoBehaviour {
 
+	//movement of float
 	public float speed;
+	//rigidbody2d of float
+	private Rigidbody2D rb2d;
+
+
+	//texts fields for showing damage done to objects
 	public Text lineDamageText;
 	public Text fishDamageText;
-	public float damageSize;
-
-	public float randomSeed;
-	public float interval;
-
-	public Text valueText;
-	public Text tempText;
-	private bool yes;
-	private float time;
-
-	private Rigidbody2D rb2d;
+	//value of damage done to objects
 	private float fishDamage;
 	private float lineDamage;
-	private bool goodPosition;
+	//damage size by every frame
+	public float damageSize;
 
+
+	//time to change fish event
+	private float interval;
+	//text field for showing time passed from last change of flag
+	public Text valueText;
+	//range values for random
+	public float minRange;
+	public float maxRange;
+
+
+	//text field for showing value of flag
+	public Text tempText;
+	//variable bool for event of fish
+	private bool fishPull;
+	//time passed from last flag change
 	private float fullTime;
+
+
+	//variable bool of showing if float is in good position
+	private bool goodPosition;
 
 	// Use this for initialization
 	void Start()
 	{
-		yes = true;
+		fishPull = true;
 		rb2d = GetComponent<Rigidbody2D>();
 		fishDamage = 0f;
 		lineDamage = 0f;
 		fullTime = 0f;
-		ChangeTime();
-		//InvokeRepeating("ChangeYes", 0f, time);
-		//Invoke("ChangeFlag", time);
+		interval = 2f;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-
-		Vector2 movement = new Vector2(moveHorizontal, transform.position.y);
-
-		rb2d.AddForce(movement * speed);
-
-		if (yes)
-		{
-			tempText.text = "True";
-		}
-		else
-		{
-			tempText.text = "False";
-		}
-
+		MoveFloat();
+		ShowFishEvent();
 		CalculateAndPrintDamage();
-
-		//Invoke("ChangeYes", time);
-		//randomSeed = Random.Range(0f, 4f);
 		ChangeFlag();
 	}
 
+	//Function controlling moving the hook
+	private void MoveFloat()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 movement = new Vector2(moveHorizontal, transform.position.y);
+		rb2d.AddForce(movement * speed);
+	}
+
+	//Function calculating and changing the time, that fish is making sth for specific period
 	private void ChangeFlag()
 	{
 
 		fullTime += Time.deltaTime;
-		valueText.text = fullTime.ToString();
+		valueText.text = "Time to change:\n" + (interval - fullTime).ToString();
 
 
 		if (interval < fullTime)
 		{
-			yes = !yes;
+			fishPull = !fishPull;
 			fullTime = 0f;
-			interval = Random.Range(5f, 10f);
+			interval = Random.Range(minRange, maxRange);
 		}
-
-		//ChangeTime(interval, random);
 	}
 
-	private void ChangeTime()
+	//Function showing if fish is pulling the hook or not
+	private void ShowFishEvent()
 	{
-		time = Random.Range(10f, 40f);
+		if (fishPull)
+		{
+			tempText.text = "Fish is pulling";
+		}
+		else
+		{
+			tempText.text = "Fish is not moving";
+		}
 	}
 
+	//Function calculating damage done to hook or fish
 	private void CalculateAndPrintDamage()
 	{
 		if (goodPosition)
@@ -96,10 +110,11 @@ public class FloatController : MonoBehaviour {
 			lineDamage += damageSize * Time.deltaTime;
 		}
 
-		fishDamageText.text = fishDamage.ToString();
-		lineDamageText.text = lineDamage.ToString();
+		fishDamageText.text = "Damage done to fish:\n" + fishDamage.ToString();
+		lineDamageText.text = "Damage done to hook:\n" + lineDamage.ToString();
 	}
 
+	//Functions checking if float is in good position
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.CompareTag("GoodBar"))
@@ -111,7 +126,6 @@ public class FloatController : MonoBehaviour {
 			goodPosition = false;
 		}
 	}
-
 	private void OnTriggerExit2d(Collider2D other)
 	{
 		if (other.gameObject.CompareTag("GoodBar"))
