856b5fce97dafc4cfbf43ddfdead6bfec029dbb6
oskar194 <worminion@gmail.com>
2017-11-02 16:31:43 +0100

    Dodano nowa scene z mechanika radaru i assety do niej

---
+++ Assets/Scripts/FishSpawnController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawnController : MonoBehaviour {
+
+	public float generationInterval;
+	public float genX;
+	public float genY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
