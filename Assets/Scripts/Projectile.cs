5f995fb2ef1f0e0c80505f82895c0608a60195d6
195083 <195083@CARBON.CTI.P.LODZ.PL>
2017-11-08 16:02:51 +0100

    add max tension and camera taracking

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,29 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour {
 
-    public Rigidbody2D rb;
+    public Rigidbody2D rb;  
+    public GameObject hook;
     public float releaseTime = .15f;
+    public float maxTension;
     private bool isPressed = false;
+    private bool cameraTracking = false;
+    private Vector3 offset;
+    private void Start()
+    {
+        offset = Camera.main.transform.position - rb.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (cameraTracking && Camera.main.transform.position.y > -2)
+        {
+            Camera.main.transform.position = rb.transform.position + offset;
+            rb.freezeRotation = false;
+        }
+    }
 
     private void Update()
     {
         if (isPressed)
         {
-            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            MoveBall();
         }
+    }
+
+    private void MoveBall()
+    {
+        float distance = Vector2.Distance(hook.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (distance < maxTension)
+        {
+            rb.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0); ;
+        }
+        else
+        {
+            rb.Sleep();
+        }
+
     }
 
     private void OnMouseDown()
     {
+        Cursor.visible = false;
         isPressed = true;
         rb.isKinematic = true;
     }
 
     private void OnMouseUp()
     {
+        Cursor.visible = true;
+        cameraTracking = true;
+        rb.WakeUp();
         isPressed = false;
         rb.isKinematic = false;
         StartCoroutine(Release());
