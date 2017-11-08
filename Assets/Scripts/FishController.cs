b98ef9c455b3ac92b2ad4df0639ae2a7559689c6
oskar194 <worminion@gmail.com>
2017-11-08 16:34:08 +0100

    Dodano nowa scene z radarem

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+
+	public float velocity;
+	public Side facingSide;
+
+	public enum Side {
+		left = -1,
+		right = 1
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3(velocity*(float)facingSide*Time.deltaTime, 0,0);
+		if (this.transform.position.x > Screen.width + 10 || this.transform.position.x < -10) {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectSpawnerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnerController : MonoBehaviour {
+
+	public float generationInterval;
+
+	public float maxFishX;
+	public float maxFishY;
+
+	public float maxRockX;
+	public float maxRockY;
+
+	public float maxBushX;
+	public float maxBushY;
+
+	public float maxFishSpeed;
+	public float minFishSpeed;
+
+	public float specialFishProbability;
+	public float obstacleProbability;
+	public float fishProbability;
+
+	public GameObject fishPrefab;
+	public GameObject obstaclePrefab;
+	private IEnumerator coroutine;
+	private Boolean over = false;
+	
+	void Start () {
+		coroutine = WaitAndGenerate(generationInterval);
+	}
+	
+
+	void Update () {
+		StartCoroutine(coroutine);
+	}
+
+	private IEnumerator WaitAndGenerate(float generationInterval) {
+		while (!over) {
+			yield return new WaitForSeconds(generationInterval);
+			GenerateRandomObject();
+			over = true;
+		}
+	}
+
+	private GameObject GenerateRandomObject() {
+		GameObject generated = GameObject.Instantiate(fishPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		return generated;
+	}
+}
